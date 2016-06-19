e3f673dbbd3df253ff58f44408a48fb7cb974e07
Sean Genabe <seangenabe@outlook.com>
2016-06-19 17:07:02 +0800

    add moves test

---
+++ test/moves.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const tap = require('tap')
+const {
+  SurrenderMove,
+  OnlineCardMove
+}
+
+tap.test('Move', t => {
+  t.test('constructor', t => {
+    let tests = [
+      [[], "first argument required"],
+      [[NaN], "first argument must be object"],
+      [[{ team: NaN }], "team must be Team"],
+    ]
+    for (let [args, errorMessage] of tests) {
+      t.throws(() => new SurrenderMove(..args), errorMessage)
+    }
+    new SurrenderMove({ team: Team.top })
+    t.end()
+  })
+  let move = new SurrenderMove({ team: Team.bottom })
+  t.ok(move.team === Team.bottom)
+  t.end()
+})
