30d88c95b51001e6dd6ff94c367930faf01a2557
Sean Genabe <seangenabe@outlook.com>
2016-06-20 15:52:42 +0800

    make TerminalCardMove.source optional again

--- src/square-move.js
+++ src/square-move.js
@@ -8,14 +8,14 @@ const Square = require('./square')
  * @classdesc Represents a move involving a board square.
  * @param {object} opts
  * @param {Symbol} opts.team Passed to {@link Move|Move constructor}
- * @param {Square} opts.square The square on which this move is operated upon.
+ * @param {Square} [opts.square] The square on which this move is operated upon.
  */
 module.exports = class SquareMove extends Move {
 
   constructor(opts) {
     super(opts)
     let { source } = opts
-    if (!(source instanceof Square)) {
+    if (!(source == null || source instanceof Square)) {
       throw new TypeError("opts.source must be a Square")
     }
     this._source = source
